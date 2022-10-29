a87fb5fc6235941fa391305890e01bdd40b04e7f
Grin <zuvado11@yandex.ua>
2022-10-29 13:51:04 +0300

    Add project files.

---
+++ PawnshopWebApp/Pages/Index.razor.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.AspNetCore.Components;
+
+namespace PawnshopWebApp.Pages;
+
+public class IndexBase : ComponentBase {
+    public string Login = "";
+    public string Password = "";
+
+    public bool? SuccessfulLogin = null;
+
+    public void TryLogin() {
+        if (!String.IsNullOrEmpty(Login) || !String.IsNullOrEmpty(Password)) {
+            if (Login == "login" && Password == "password") {
+                SuccessfulLogin = true;
+            }
+            else {
+                SuccessfulLogin = false;
+            }
+        }
+    }
+}
