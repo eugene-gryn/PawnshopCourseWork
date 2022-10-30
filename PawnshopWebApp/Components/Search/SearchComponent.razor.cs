3750d7b0c5862b228281c6f5f5f329c590f06377
Grin <zuvado11@yandex.ua>
2022-10-30 22:49:30 +0200

    Элементи були гарно розташовані та впорядковані

---
+++ PawnshopWebApp/Components/Search/SearchComponent.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace PawnshopWebApp.Components.Search;
+
+public partial class SearchComponent {
+    [Parameter]
+    public List<string> Filters { get; set; }
+
+    [Parameter]
+    public int SelectedIndex { get; set; }
+
+    [Parameter]
+    public EventCallback<int> OnFilterChanged { get; set; }
+
+    [Parameter]
+    public EventCallback<string?> OnQueryChanged { get; set; }
+
+
+    public void OnChangedIndex(string element) {
+        var index = Filters.IndexOf(element);
+        SelectedIndex = index;
+        OnFilterChanged.InvokeAsync(index);
+    }
+
+    public void OnChangedInputQuery(string? query) {
+        OnQueryChanged.InvokeAsync(query);
+    }
+}
---
+++ PawnshopWebApp/Components/Search/SortingComponent.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace PawnshopWebApp.Components.Search;
+
+public partial class SortingComponent {
+    [Parameter]
+    public List<string> Filters { get; set; }
+
+    [Parameter]
+    public int SelectedIndex { get; set; }
+
+    [Parameter]
+    public EventCallback<int> OnFilterChanged { get; set; }
+    
+
+    public void OnChangedIndex(string attr) {
+        var index = Filters.IndexOf(attr);
+        SelectedIndex = index;
+        OnFilterChanged.InvokeAsync(index);
+    }
+}
